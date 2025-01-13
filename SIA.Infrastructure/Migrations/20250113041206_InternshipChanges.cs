f98263f2b70fb35c12eaaa4ca36aab7b8989f833
cristiifrim <i.cristian02@yahoo.com>
2025-01-13 06:26:52 +0200

    added configs

---
+++ SIA.Infrastructure/Migrations/20250113041206_InternshipChanges.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SIA.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InternshipChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ApplicationEndline",
+                table: "Internships",
+                newName: "ApplicationDeadline");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Internships",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Internships");
+
+            migrationBuilder.RenameColumn(
+                name: "ApplicationDeadline",
+                table: "Internships",
+                newName: "ApplicationEndline");
+        }
+    }
+}
